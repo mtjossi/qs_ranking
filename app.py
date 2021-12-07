a16f4d17daab37ca3cb215cfddf04688bb12740b
mtjossi <45386128+mtjossi@users.noreply.github.com>
2021-12-07 15:08:56 +0800

    added graph

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
 
 st.title('QS Ranking')
 
@@ -16,10 +18,41 @@ st.dataframe(df2['Country'].value_counts())
 # st.write(df2['Country'].value_counts)
 
 st.write('-----')
-st.write('Historical Rankings')
+st.write('Historical Rankings of Top 25 Universities')
 df3 = df.copy()
 df3 = df3.head(25)
 df3 = df3[['Current Ranking', '2021 Ranking', '2020 Ranking', '2019 Ranking','University Name']]
 df3 = df3[df3.columns[::-1]]
 
 st.dataframe(df3)
+
+
+
+df3['Current Ranking'] = df3['Current Ranking'].apply(lambda x: x.replace('=', ''))
+years = [2019, 2020, 2021, 2022]
+rank = np.arange(1,26)
+
+uni_to_plot = st.selectbox("Plot historical Ranking of:", df3['University Name'].to_list())
+ranking_dict = dict(zip(df3['University Name'], df3.index))
+
+fig = go.Figure(
+    data=[go.Scatter(x=years, y=[int(r) for r in df3.iloc[ranking_dict[uni_to_plot], 1:]])],
+    layout=go.Layout(
+        xaxis=dict(range=[2019, 2022], autorange=True),
+        title=f"{df3.iloc[ranking_dict[uni_to_plot], 0]} Historical Ranking"
+    )
+)
+fig.update_layout(
+xaxis = dict(
+    tickmode = 'linear',
+    tick0 = 2019,
+    dtick = 1
+),
+    yaxis = dict(
+    tickmode = 'linear',
+    dtick = 1,
+    autorange="reversed"
+)
+)
+
+st.plotly_chart(fig)
